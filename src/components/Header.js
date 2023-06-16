995f432fdff71efa4adaf8e2d92fc4162ed65ddd
Muhammad Danish <danishpk175@gmail.com>
2023-06-16 07:50:12 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ function Header() {
             <div className="header__title">My Portfolio</div>
             <nav className="header__nav">
                 <ul className="header__nav-list">
-                    <li className="header__nav-item"><a href="#">Home</a></li>
+                    <li className="header__nav-item"><a href="#home">Home</a></li>
                     <li className="header__nav-item"><a href="#about">About</a></li>
                     <li className="header__nav-item"><a href="#resume">Resume</a></li>
                     <li className="header__nav-item"><a href="#projects">Projects</a></li>
